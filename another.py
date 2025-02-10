fefbaf3f1fea9efb76f14ba92dbda3bbaf73e4e4
jacobgeisen <jacobgeisen@gmail.com>
2025-02-10 15:38:42 -0600

    new python file

---
+++ another.py
@@ -0,0 +1,13 @@
+def my_decorator(func):
+    def wrapper(*args):
+        print("Before calling the function")
+        result = func(*args)
+        print("After calling the function")
+        return result
+    return wrapper
+
+@my_decorator
+def greet(name):
+    print("Hello," + name + "!")
+
+greet("Alice")
